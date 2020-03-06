2194e41fe3cb747bd4cea16118672323911f7a24
Brian Price <brianprice88@gmail.com>
2020-03-05 16:30:57 -0800

    "queries working with front-end requests.  SEtting up mutations next"

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import gql from 'graphql-tag'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:8000/graphql'
+  }),
+  cache: new InMemoryCache()
+});
+
+  class App extends Component {
+    state = {
+      data: ''
+    }
+    componentDidMount(){
+    client.query({
+      query: gql `query {
+        areas {
+          name
+          latitude
+          longitude
+          radius
+          enter
+          exit
+          title
+          body
+        }
+      }`
+    }).then(response => this.setState({
+      data: response.data.areas
+    }, () => console.log(this.state)))   
+  }
+    
+    render() {
+      return (
+        <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+      <Text style={styles.headerText}>Text</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </View>
+        </ApolloProvider>
+      )
+    }
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +58,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
