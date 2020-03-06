3a0f4505a7ecf7062394e6396c314b1d0c876f60
Brian Price <brianprice88@gmail.com>
2020-03-05 19:43:17 -0800

    "Write all CRUD operations on front end - just need to put them in proper place in the actual app, and wrap in functions that will accept parameters, besides the getAllAreas to run on ComponentDidMount"

--- App.js
+++ App.js
@@ -16,24 +16,130 @@ const client = new ApolloClient({
       data: ''
     }
     componentDidMount(){
-    client.query({
-      query: gql `query {
-        areas {
-          name
-          latitude
-          longitude
-          radius
-          enter
-          exit
-          title
-          body
-        }
-      }`
-    }).then(response => this.setState({
-      data: response.data.areas
-    }, () => console.log(this.state)))   
-  }
+
+      //get all areas   
+
+  //  client.query({
+  //     query: gql `
+  //     query areas {
+  //       areas {
+  //         name
+  //         latitude
+  //         longitude
+  //         radius
+  //         enter
+  //         exit
+  //         title
+  //         body
+  //       }
+  //     }`
+  //   }).then(response => this.setState({
+  //     data: response.data.areas
+  //   }, () => console.log(this.state)));
     
+    //get one area's information based on its name
+
+    // client.query({
+    //   variables: {"name": "mongo"},
+    //   query: gql `
+    //   query area ($name: String!) {
+    //     area (name: $name) {
+    //       name
+    //       latitude
+    //       longitude
+    //       radius
+    //       enter
+    //       exit
+    //       title
+    //       body
+    //     }
+    //   }`
+    // }).then(response => this.setState({
+    //   data: response.data.area
+    // }, () => console.log(this.state)));
+
+     //create an Area --> must enter all parameters name/latitude/longitude/radius/enter/exit/title/body
+
+    // client.mutate({
+    //  variables: {"name": "mongo", "latitude": 1212.1, "longitude": -121.2, "radius": 20, "enter": true, "exit": true, "title": "welcome", "body": "youve got mail"},
+    //  mutation: gql`
+    //  mutation createArea ($name: String!, $latitude: Float!, $longitude: Float!, $radius: Int!, $enter: Boolean!, $exit: Boolean!, $title: String!, $body: String!) {
+    //    createArea (name: $name, latitude: $latitude, longitude: $longitude, radius: $radius, enter: $enter, exit: $exit, title: $title, body: $body) {
+    //      name
+    //      latitude
+    //      longitude
+    //      radius
+    //      enter
+    //      exit
+    //      title
+    //      body
+    //    }
+    //  }`
+    // }).then(response => console.log(response))
+    // .catch(err => console.error(err));
+  
+
+  //update an Area -- must include name, then optional latitude/longitude/radius
+
+  // client.mutate({
+  //   variables: {"name": "hello", "latitude": -12.2, "longitude": -40.2, "radius": 199},
+  //   mutation: gql`
+  //   mutation updateArea ($name: String!, $latitude: Float, $longitude: Float, $radius: Int) {
+  //     updateArea (name: $name, latitude: $latitude, longitude: $longitude, radius: $radius) {
+  //       name
+  //       latitude
+  //       longitude
+  //       radius
+  //     }
+  //   }`
+  //  }).then(response => console.log(response))
+  //  .catch(err => console.error(err))
+
+  //delete an Area based on its name
+
+  // client.mutate({
+  //   variables: {"name": "mongo"},
+  //   mutation: gql`
+  //   mutation deleteArea ($name: String!) {
+  //     deleteArea (name: $name) {
+  //       name
+  //     }
+  //   }`
+  //  }).then(response => console.log(response))
+  //  .catch(err => console.error(err));
+
+  //update an Event -- must include area name, then optional enter/exit/title/goodbye parameters
+
+  //  client.mutate({
+  //   variables: {"name": "hello", "enter": false, "exit": false, "title": "goodbye", "body": "have a nice day"},
+  //   mutation: gql`
+  //   mutation updateEvent ($name: String!, $enter: Boolean!, $exit: Boolean!, $title: String!, $body: String!) {
+  //     updateEvent (name: $name, enter: $enter, exit: $exit, title: $title, body: $body) {
+  //       name
+  //       enter
+  //       exit
+  //       title
+  //       body
+  //     }
+  //   }`
+  //  }).then(response => console.log(response))
+  //  .catch(err => console.error(err))
+
+  //delete an Event based on the name of the area
+
+  // client.mutate({
+  //   variables: {"name": "hello"},
+  //   mutation: gql`
+  //   mutation deleteEvent ($name: String!) {
+  //     deleteEvent (name: $name) {
+  //       name
+  //     }
+  //   }`
+  //  }).then(response => console.log(response))
+  //  .catch(err => console.error(err));
+
+
+};
     render() {
       return (
         <ApolloProvider client={client}>
